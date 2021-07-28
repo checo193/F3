b1990c93d13b4e76b78afb794b2a38817c8cc375
Daniel Cassell <daniel.daniel.cc97@gmail.com>
2021-07-28 15:44:11 +0100

    feat: add links to title and create teams button
    clicking each link takes user to the respective url path

--- f3/src/components/Header.js
+++ f3/src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header>
-      <div>
-        <h1>F3</h1>
-      </div>
+      <Link to={'/'}>
+        <div>
+          <h1>F3</h1>
+        </div>
+      </Link>
       <div className='btnContainer'>
-        <button className='createTeamBtn'>Create Team</button>
+        <Link to={'/teams'}>
+          <button className='createTeamBtn'>Create Team</button>
+        </Link>
       </div>
     </header>
   );
