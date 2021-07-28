3e4161a9072bd78358f1a213a5aec716665433ee
Daniel Cassell <daniel.daniel.cc97@gmail.com>
2021-07-28 14:11:49 +0100

    style(player card): fix positioning of stats and title

--- f3/src/components/Player.js
+++ f3/src/components/Player.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-export default function Player({player}) {
+export default function Player({ player }) {
   return (
     <div className='player'>
-      <h3>{player.name}</h3>
-      <h6>Goals: {player.goals}</h6>
-      <h6>Games: {player.games}</h6>
-      <h6>Wins: {player.wins}</h6>
-      <h6>Losses: {player.losses}</h6>
-      <h6>Motms: {player.motms}</h6>
+      <div className='playerName'>
+        <h3>{player.name}</h3>
+      </div>
+      <div className='playerStats'>
+        <div className='playerStats1'>
+          <h6 className='stat'>Games: {player.games}</h6>
+          <h6 className='stat'>Goals: {player.goals}</h6>
+          <h6 className='stat'>Wins: {player.wins}</h6>
+        </div>
+        <div className='playerStats2'>
+          <h6 className='stat'>Losses: {player.losses}</h6>
+          <h6 className='stat'>Motms: {player.motms}</h6>
+        </div>
+      </div>
     </div>
   );
 }
