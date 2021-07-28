4d2d1379c73036e61248e91f954557d800222ebe
Daniel Cassell <daniel.daniel.cc97@gmail.com>
2021-07-28 12:04:00 +0100

    feat(squad): add display squad function

--- f3/src/containers/Squad.js
+++ f3/src/containers/Squad.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Player from '../components/Player'
+import json from '../json-database/players.json'
+
 
 export default function Squad() {
+
+  const [squad, setSquad] = useState(json);
+
+  const displaySquad = () => {
+    return squad.map((player) => {
+      return <Player player={player} />
+    })
+  }
+  
   return (
     <>
     <div>
       <h3>Squad Renders</h3>
     </div>
     <div>
-      <Player />
+      {displaySquad()}
     </div>
     </>
   )
