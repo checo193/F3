1817563778e40c8f572083f7e8e40db836605694
Daniel Cassell <daniel.daniel.cc97@gmail.com>
2021-07-28 12:10:33 +0100

    feat(player): display player stats
    the displaySquad function now displays each player and their individual stats in the Player component

--- f3/src/components/Player.js
+++ f3/src/components/Player.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function Player() {
+export default function Player({player}) {
   return (
-    <div>
-        <h3>Player Renders</h3>
+    <div className='player'>
+      <h3>{player.name}</h3>
+      <h6>Goals: {player.goals}</h6>
+      <h6>Games: {player.games}</h6>
+      <h6>Wins: {player.wins}</h6>
+      <h6>Losses: {player.losses}</h6>
+      <h6>Motms: {player.motms}</h6>
     </div>
-  )
+  );
 }
