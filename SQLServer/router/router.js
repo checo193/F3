95acafa3a0c0c31d78335c56f24d78e380c60740
Sergio Azizi <sergio.azizi.14@gmail.com>
2021-08-06 18:16:53 +0100

    feat: controller

--- SQLServer/router/router.js
+++ SQLServer/router/router.js
@@ -1,39 +1,13 @@
-const { Player } = require("../models/index");
+const {
+  getPlayers,
+  addPlayer,
+  updatePlayer,
+} = require("../controller/controller");
 
 const router = require("express").Router();
 
-router.get("/", async (req, res) => {
-  res.send("hello app.get");
-});
-
-router.get("/squad", async (req, res) => {
-  try {
-    const squad = await Player.findAll();
-    console.log(squad);
-    res.send(squad);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-router.post("/player", async (req, res) => {
-  try {
-    const newPlayer = await Player.create(req.body);
-    res.send(newPlayer).status(201);
-  } catch (err) {
-    console.error(err);
-  }
-});
-
-router.put("/player/:id", async (req, res) => {
-  try {
-    const updatedPlayer = await Player.update(req.body, {
-      where: { id: req.params.id },
-    });
-    res.send("player was updated").status(201);
-  } catch (err) {
-    console.error(err);
-  }
-});
+router.get("/squad", getPlayers);
+router.post("/player", addPlayer);
+router.put("/player/:id", updatePlayer);
 
 module.exports = router;
