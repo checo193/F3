affcee76e106cdd1cf5055c3e7938ed7bb231e46
Sergio Azizi <sergio.azizi.14@gmail.com>
2021-08-06 13:57:50 +0100

    notes from help request

--- SQLServer/index.js
+++ SQLServer/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const pool = require('./db');
+const cors = require("cors");
+const pool = require("./db");
 
 const PORT = 5000;
 
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // create a player
 
-app.post('/player', async (req, res) => {
+app.post("/player", async (req, res) => {
   try {
     const {
       name,
@@ -27,7 +27,7 @@ app.post('/player', async (req, res) => {
       imageUrl,
     } = req.body;
     const newPlayer = await pool.query(
-      'INSERT INTO squad (name, games, wins, losses, goals, motms, teamGoals, goalsConceded, imageUrl) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *',
+      "INSERT INTO squad (name, games, wins, losses, goals, motms, teamGoals, goalsConceded, imageUrl) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
       [
         name,
         games,
@@ -48,9 +48,9 @@ app.post('/player', async (req, res) => {
 
 //get all players (squad);
 
-app.get('/squad', async (req, res) => {
+app.get("/squad", async (req, res) => {
   try {
-    const squad = await pool.query('SELECT * FROM squad');
+    const squad = await pool.query("SELECT * FROM squad");
     res.json(squad.rows);
   } catch (err) {
     console.log(err.message);
@@ -59,7 +59,7 @@ app.get('/squad', async (req, res) => {
 
 // update a player
 
-app.put('/player/:id', async (req, res) => {
+app.put("/player/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const {
@@ -76,59 +76,59 @@ app.put('/player/:id', async (req, res) => {
 
     if (name) {
       const updateName = await pool.query(
-        'UPDATE squad SET name = $1 WHERE id = $2',
+        "UPDATE squad SET name = $1 WHERE id = $2",
         [name, id]
       );
     }
     if (games || games === 0) {
       const updateGames = await pool.query(
-        'UPDATE squad SET games = $1 WHERE id = $2',
+        "UPDATE squad SET games = $1 WHERE id = $2",
         [games, id]
       );
     }
     if (wins || wins === 0) {
       const updateWins = await pool.query(
-        'UPDATE squad SET wins = $1 WHERE id = $2',
+        "UPDATE squad SET wins = $1 WHERE id = $2",
         [wins, id]
       );
     }
     if (losses || losses === 0) {
       const updateLosses = await pool.query(
-        'UPDATE squad SET losses = $1 WHERE id = $2',
+        "UPDATE squad SET losses = $1 WHERE id = $2",
         [losses, id]
       );
     }
     if (goals || goals === 0) {
       const updateGoals = await pool.query(
-        'UPDATE squad SET goals = $1 WHERE id = $2',
+        "UPDATE squad SET goals = $1 WHERE id = $2",
         [goals, id]
       );
     }
     if (motms || motms === 0) {
       const updateMotms = await pool.query(
-        'UPDATE squad SET motms = $1 WHERE id = $2',
+        "UPDATE squad SET motms = $1 WHERE id = $2",
         [motms, id]
       );
     }
     if (teamGoals || teamGoals === 0) {
       const updateTeamGoals = await pool.query(
-        'UPDATE squad SET teamGoals = $1 WHERE id = $2',
+        "UPDATE squad SET teamGoals = $1 WHERE id = $2",
         [teamGoals, id]
       );
     }
     if (goalsConceded || goalsConceded === 0) {
       const updateGoalsConceded = await pool.query(
-        'UPDATE squad SET goalsConceded = $1 WHERE id = $2',
+        "UPDATE squad SET goalsConceded = $1 WHERE id = $2",
         [goalsConceded, id]
       );
     }
     if (imageUrl) {
       const updateImageUrl = await pool.query(
-        'UPDATE squad SET imageUrl = $1 WHERE id = $2',
+        "UPDATE squad SET imageUrl = $1 WHERE id = $2",
         [imageUrl, id]
       );
     }
-    res.json('Player was updated');
+    res.json("Player was updated");
   } catch (err) {
     console.log(err.message);
   }
---
+++ SQLServer/scripts/seed.js
@@ -0,0 +1,9 @@
+// import sequelize
+
+// (async () => {
+//     //connect to db using sequelize.sync / sequelize.auth
+//     // set force to true
+
+//     bulkcreate(data)
+//     // have some kind of confimation that db has been populated
+// })()
