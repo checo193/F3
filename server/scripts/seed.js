38da90a7008ca6512613f4df5c10354501835eb1
marishibata <76498339+marishibata@users.noreply.github.com>
2021-08-06 19:20:30 +0100

    feat: seed in server

--- server/scripts/seed.js
+++ server/scripts/seed.js
@@ -1,9 +1,31 @@
-// import sequelize
+const Player = require('../models/index');
 
-// (async () => {
-//     //connect to db using sequelize.sync / sequelize.auth
-//     // set force to true
+const presetData = [{
+  name: "john",
+  games: 2,
+  wins: 1,
+  losses: 1,
+},
+{
+  name: "paul",
+  games: 2,
+  wins: 2,
+  losses: 0,
+},
+{
+  name: "becky",
+  games: 2,
+  wins: 0,
+  losses: 2,
+}
+];
 
-//     bulkcreate(data)
-//     // have some kind of confimation that db has been populated
-// })()
+(async () => {
+  //connect to db using sequelize.sync / sequelize.auth
+  await Player.sequelize.sync({ force: true });
+  // set force to true
+
+  Player.bulkCreate(presetData);
+  // have some kind of confimation that db has been populated
+})()
+
